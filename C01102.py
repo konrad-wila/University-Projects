66c1d299ed308933d928455c9f0546e113d99b36
Huzaifa Sabahuddin <huzaifasabah@gmail.com>
2023-01-14 11:36:08 +0000

    CO1107 Workshop

--- C01102.py
+++ C01102.py
@@ -20,6 +20,7 @@ def letter_counter(message: str):
 
 def dict_to_tuples():
     converted = [(k, v) for k, v in letter_dict.items()]
+    # converted = List(letterdict.items())
     return converted
 
 
@@ -47,6 +48,46 @@ def get_product(text: str):
 
 if __name__ == "__main__":
     print(get_product("4*3*2*1"))
+
+
+
+#Task 3
+
+
+class Student:
+    def __init__(self, student_name=" ", student_id=0, student_age=0, student_marks=0):
+        self.student_id = student_id
+        self.student_age = student_age
+        self.student_marks = student_marks
+        self.student_name = student_name
+
+    def set_student_age(self, sage):
+        self.student_age = sage
+
+    def get_student_age(self):
+        return self.student_age
+
+    def student_marks(self, mark):
+        self.student_marks = mark
+
+    def get_student_marks(self):
+        return self.student_marks
+
+    def __str__(self):
+        return f"Student {self.student_name} of age {self.student_age} has student id {self.student_id} and has attained {self.student_marks} marks"
+
+
+
+
+if __name__ == '__main__':
+    s = Student
+    name = str(input("Enter name of the student: "))
+    sid = int(input("Students id: "))
+    age = int(input("Student age: "))
+    marks = int(input("Student marks: "))
+    print(s(name, sid, age, marks))
+    s.set_student_age(sid)
+
 
 
 
