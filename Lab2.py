1865718a10789929c20f92a2f03ffdc65fca7d74
Huzaifa Sabahuddin <huzaifasabah@gmail.com>
2023-01-17 22:37:00 +0000

    Sorting Algorithm

--- Lab2.py
+++ Lab2.py
@@ -1,18 +1,24 @@
 #Task 1
 def listed(file):
     arr = []
-    with open(file, 'r') as f:
-        for line in f:
-            line = line.strip()
-            line = [float(x) for x in line.split(',')]
-            arr.append(line)
-        return arr
+    try:
+        with open(file, 'r') as f:
+            for line in f:
+                line = line.strip()
+                line = [float(x) for x in line.split(',')]
+                arr.append(line)
+            return arr
+    except FileNotFoundError:
+        print("File not found: %s" % file)
 
 
 def printed():
-    a = listed("Tiny.txt")
-    for i in range(len(a)):
-        print(f"Miles {a[i][0]},£ {a[i][1]}")
+    try:
+        a = listed("Tiny.txt")
+        for i in range(len(a)):
+            print(f"Miles {a[i][0]},£ {a[i][1]}")
+    except Exception as e:
+        print("Some error occurred", e)
 
 
 print(listed("Tiny.txt"))
