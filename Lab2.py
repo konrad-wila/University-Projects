2e168c6411b9176cc73053ebb3ee76b4bba8b9aa
Huzaifa Sabahuddin <huzaifasabah@gmail.com>
2023-01-18 18:33:24 +0000

    Sorting Algorithm

--- Lab2.py
+++ Lab2.py
@@ -53,6 +53,44 @@ def selectionSortPrice(values):
 # we can also use sort() function as values.sort(key=lambda x:x[1])
 
 
+#Extra
+def bubblesortdistance(values):
+    for i in range(len(values)):
+        for j in range(0, len(values)-i-1):
+            if values[j][0] > values[j+1][0]:
+                values[j],values[j+1] = values[j+1],values[j]
+    return values
+
+def bubblesortprice(values):
+    for i in range(len(values)):
+        for j in range(0, len(values)-i-1):
+            print(j)
+            if values[j][1] > values[j+1][1]:
+                values[j],values[j+1] = values[j+1],values[j]
+    return values
+
+
+def insertionsortdistance(values):
+    for i in range(1,len(values)-1):
+        value_to_sort = values[i][0]
+        while values[i-1][0] > value_to_sort and i > 0:
+            values[i],values[i-1] = values[i-1],values[i]
+            i = i-1
+    return values
+
+
+def insertionsortprice(values):
+    for i in range(1,len(values)-1):
+        value_to_sort = values[i][1]
+        while values[i-1][1] > value_to_sort and i > 0:
+            values[i],values[i-1] = values[i-1],values[i]
+            i = i-1
+    return values
+
+
+
+
+
 if __name__ == "__main__":
     printed(listed("Tiny.txt"))
     print("Sorting according to Distance: ")
@@ -60,7 +98,7 @@ if __name__ == "__main__":
     print("Sorting according to Price: ")
     printed(selectionSortPrice(listed("Tiny.txt")))
     print(listed("Tiny.txt"))
-
+    print(bubblesortprice(listed("Tiny.txt")))
 
 
 
