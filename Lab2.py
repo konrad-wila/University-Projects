e63180aeac820be796f8ae158c273c0ceef8a1e6
Huzaifa Sabahuddin <huzaifasabah@gmail.com>
2023-01-18 16:54:21 +0000

    Sorting Algorithm

--- Lab2.py
+++ Lab2.py
@@ -12,17 +12,42 @@ def listed(file):
         print("File not found: %s" % file)
 
 
-def printed():
+def printed(value):
     try:
-        a = listed("Tiny.txt")
-        for i in range(len(a)):
-            print(f"Miles {a[i][0]},£ {a[i][1]}")
+        for i in range(len(value)):
+            print(f"Miles {value[i][0]},£ {value[i][1]}")
     except Exception as e:
         print("Some error occurred", e)
 
 
-print(listed("Tiny.txt"))
-printed()
-
-
 # Task 2
+def selectionSortDistance(values):
+    for i in range(len(values)-1):
+        min_pos = i
+        for j in range(i+1, len(values)):
+            if values[j][0] < values[min_pos][0]:
+                min_pos = j
+        values[i], values[min_pos] = values[min_pos], values[i]
+    return values
+
+
+def selectionSortPrice(values):
+    for i in range(len(values)-1):
+        min_pos = i
+        for j in range(i+1, len(values)):
+            if values[j][1] < values[min_pos][1]:
+                min_pos = j
+        values[i], values[min_pos] = values[min_pos], values[i]
+    return values
+
+
+if __name__ == "__main__":
+    printed(listed("Tiny.txt"))
+    print("Sorting according to Distance: ")
+    printed(selectionSortDistance(listed("Tiny.txt")))
+    print("Sorting according to Price: ")
+    printed(selectionSortPrice(listed("Tiny.txt")))
+
+
+
+
